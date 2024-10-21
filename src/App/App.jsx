06dacd1823a755f4e6cfd5d6ce29980ee24e8157
Julio César Hernández Mendoza <juliohernandezcm@gmail.com>
2024-10-21 22:05:59 +0200

    feat: initial frontend structure setup

---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<div>
+			<h1
+				className='text-4xl h-1 m-4 p-4 text-center align-middle
+         '
+			>
+				My Vanitys
+			</h1>
+			<p>This site is under construction. Stay tuned!</p>
+		</div>
+	);
+}
+export { App };
