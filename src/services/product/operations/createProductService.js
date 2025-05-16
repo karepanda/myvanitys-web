f9c843888414e2c93c6049ad92b155209cc02a3b
kennyjpc <kennyjpc@ext.inditex.com>
2025-05-16 18:58:45 +0200

    VANAITYS19: Add send token in createProductService

--- src/services/product/operations/createProductService.js
+++ src/services/product/operations/createProductService.js
@@ -3,7 +3,16 @@ import { productApiAdapter } from '../adapters/productApiAdapter';
 
 export const createProductService = {
   createProduct: async (token, productData, errorHandler) => {
+    
     try {
+
+    console.log('Token used for authentication:', token ? 'Token present, first characters: ' + token.substring(0, 10) + '...' : 'Empty or null token');
+    
+    //Verify token format
+    if (token && !token.startsWith('Bearer ')) {
+      console.warn('The token does not have the prefix “Bearer ”. Adding the prefix...');
+      token = `Bearer ${token}`;
+    }
       const apiData = {
         name: productData.name,
         brand: productData.brand,
